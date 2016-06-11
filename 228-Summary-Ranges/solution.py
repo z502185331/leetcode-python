d6bec82b38e19ff09095ba024495262f9cc52ee7
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-11 13:12:25 -0400

    [228. Summary Ranges][Accepted]committed by LeetCode Extension

---
+++ 228-Summary-Ranges/solution.py
@@ -0,0 +1,29 @@
+class Solution(object):
+    def summaryRanges(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[str]
+        """
+        
+        if not nums:
+            return []
+        
+        ans = []
+        m = len(nums)
+        nums.append(sys.maxint) # add a elem to prevent index out of bound
+        start, end = 0, 0
+        for i in xrange(m):
+            if nums[i + 1] == nums[i] + 1:
+                end = i + 1
+            else:
+                if start == end:
+                    ans.append(str(nums[start]))
+                else:
+                    ans.append(str(nums[start]) + '->' + str(nums[end]))
+                start = end = i + 1
+        
+        # deal with the situation when nums[-2] == sys.maxint - 1
+        if start != end:
+            ans.append(str(nums[start]) + '->' + str(nums[end - 1]))
+        
+        return ans
