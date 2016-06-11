b49641d82b6c0622b88cb3ef3058c184ea9b439c
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-11 13:40:26 -0400

    [224. Basic Calculator][Accepted]committed by LeetCode Extension

--- 224-Basic-Calculator/solution.py
+++ 224-Basic-Calculator/solution.py
@@ -14,6 +14,13 @@ class Solution(object):
         stack_oper = []
         stack_num = []
         
+        def cal_helper():
+            oper = stack_oper.pop()
+            num2 = stack_num.pop()
+            num1 = stack_num.pop()
+            ans = num1 + num2 if oper == '+' else num1 - num2
+            stack_num.append(ans)
+        
         i = 0
         while i < m:
             # when cur is a digit
@@ -33,32 +40,20 @@ class Solution(object):
             # when cur is a oper
             if s[i] == ')':
                 while stack_oper[-1] != '(':
-                    oper = stack_oper.pop()
-                    num2 = stack_num.pop()
-                    num1 = stack_num.pop()
-                    ans = num1 + num2 if oper == '+' else num1 - num2
-                    stack_num.append(ans)
+                    cal_helper()
                 stack_oper.pop()
             
             elif s[i] == '(':
                 stack_oper.append('(')
             else:
                 while stack_oper and stack_oper[-1] != '(':
-                    oper = stack_oper.pop()
-                    num2 = stack_num.pop()
-                    num1 = stack_num.pop()
-                    ans = num1 + num2 if oper == '+' else num1 - num2
-                    stack_num.append(ans)
+                    cal_helper()
                 stack_oper.append(s[i])
             i +=1
         
         
         while stack_oper:
-            oper = stack_oper.pop()
-            num2 = stack_num.pop()
-            num1 = stack_num.pop()
-            ans = num1 + num2 if oper == '+' else num1 - num2
-            stack_num.append(ans)
+            cal_helper()
         
         return stack_num[-1] if stack_num else 0
                     
