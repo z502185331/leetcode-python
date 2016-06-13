e6a84e9ad762b5d9420a8d4280a10c6c2d13cda9
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-13 19:26:47 -0400

    [236. Lowest Common Ancestor of a Binary Tree][Accepted]committed by LeetCode Extension

--- 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.py
+++ 236-Lowest-Common-Ancestor-of-a-Binary-Tree/solution.py
@@ -14,34 +14,16 @@ class Solution(object):
         :rtype: TreeNode
         """
         
-        if root is None:
-            return None
+        if root is None or (root is p or root is q):
+            return root
         
-        left_flag = self.find(root.left, p, q)
-        right_flag = self.find(root.right, p, q)
+        left_node = self.lowestCommonAncestor(root.left, p, q)
+        right_node = self.lowestCommonAncestor(root.right, p, q)
         
-        if root in [p, q]:
-            if left_flag or right_flag:
-                return root
-        else:
-            if left_flag and right_flag:
-                return root
-            elif left_flag:
-                return self.lowestCommonAncestor(root.left, p, q)
-            elif right_flag:
-                return self.lowestCommonAncestor(root.right, p, q)
+        if left_node is not None and right_node is not None:
+            return root
+        elif left_node is not None:
+            return left_node
+        elif right_node is not None:
+            return right_node
         
-        
-        
-    
-    '''
-    A method to check whether the node is in the tree
-    '''
-    def find(self, root, p, q):
-        if root is None:
-            return False
-            
-        if root is p or root is q:
-            return True
-        
-        return self.find(root.left, p, q) or self.find(root.right, p, q)
