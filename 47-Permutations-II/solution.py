f901665d8e2bee1fc41df3f1c70d76735b91dc6d
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-14 20:04:30 -0400

    [47. Permutations II][Accepted]committed by LeetCode Extension

--- 47-Permutations-II/solution.py
+++ 47-Permutations-II/solution.py
@@ -9,7 +9,20 @@ class Solution(object):
             return []
             
         res = []
-        self.helper(res, [], nums)
+        nums.sort()
+        size = len(nums)
+        
+        def dfs(nums, path):
+            if len(path) == size:
+                res.append(path)
+            
+            for i in xrange(len(nums)):
+                if i > 0 and nums[i - 1] == nums[i]:
+                    continue
+                
+                dfs(nums[: i] + nums[i + 1 :], path + [nums[i]])
+        
+        dfs(nums, [])
         return res
         
     def helper(self, result, path,  nums):
