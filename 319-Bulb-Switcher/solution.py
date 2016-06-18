1fa1d2bfcc513acb783e2a16b9d0d2ac97838aba
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-18 10:30:19 -0400

    [319. Bulb Switcher][Accepted]committed by LeetCode Extension

--- 319-Bulb-Switcher/solution.py
+++ 319-Bulb-Switcher/solution.py
@@ -5,18 +5,19 @@ class Solution(object):
         :rtype: int
         """
         
-        if n == 0:
-            return 0
+        # if n == 0:
+        #     return 0
         
-        count = 0   # count how many bomb are on
-        for i in xrange(1, n + 1):
-            local_cnt = 0
-            for j in xrange(1, i + 1):
-                if i % j == 0:
-                    local_cnt += 1
+        # count = 0   # count how many bomb are on
+        # for i in xrange(1, n + 1):
+        #     local_cnt = 0
+        #     for j in xrange(1, i + 1):
+        #         if i % j == 0:
+        #             local_cnt += 1
                 
-            if (local_cnt & 1) == 1:
-                count += 1
+        #     if (local_cnt & 1) == 1:
+        #         count += 1
         
-        return count
+        # return count
+        return int(math.sqrt(n))
             
