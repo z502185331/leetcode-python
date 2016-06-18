2f2136d9878bcdb045051567f92b7e93b986d5f7
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-18 10:27:45 -0400

    [319. Bulb Switcher][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 319-Bulb-Switcher/solution.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def bulbSwitch(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        
+        if n == 0:
+            return 0
+        
+        count = 0   # count how many bomb are on
+        for i in xrange(1, n + 1):
+            local_cnt = 0
+            for j in xrange(1, i + 1):
+                if i % j == 0:
+                    local_cnt += 1
+                
+            if (local_cnt & 1) == 1:
+                count += 1
+        
+        return count
+            
