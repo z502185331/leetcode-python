fe3bcc3fcfe32346ca054642c008ba788ff5e896
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-19 13:41:34 -0400

    [62. Unique Paths][Accepted]committed by LeetCode Extension

--- 62-Unique-Paths/solution.py
+++ 62-Unique-Paths/solution.py
@@ -6,10 +6,18 @@ class Solution(object):
         :rtype: int
         """
         
-        dp = [[0] * (n + 1) for i in xrange(m + 1)]
-        dp[0][1] = 1
-        for i in xrange(m):
-            for j in xrange(n):
-                dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1]
+        # dp = [[0] * (n + 1) for i in xrange(m + 1)]
+        # dp[0][1] = 1
+        # for i in xrange(m):
+        #     for j in xrange(n):
+        #         dp[i + 1][j + 1] = dp[i + 1][j] + dp[i][j + 1]
         
-        return dp[m][n]
+        # return dp[m][n]
+        
+        dp = [1] * n
+        
+        for i in xrange(1, m):
+            for j in xrange(1, n):
+                dp[j] += dp[j - 1]
+        
+        return dp[-1]
