b6d94da1996de7668b5253a90592fa7f94fb8dd1
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-21 20:56:28 -0400

    [78. Subsets][Accepted]committed by LeetCode Extension

---
+++ 78-Subsets/solution.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def subsets(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        
+        if nums is None:
+            return []
+        result = []
+        self.dfs(result, sorted(nums), 0, [])
+        return result
+    
+    def dfs(self, result, S, index, path):
+        if path not in result:
+            result.append(list(path))
+        if index == len(S):
+            return
+        
+        for i in range(index, len(S)):
+            path.append(S[i])
+            self.dfs(result, S, i + 1, path)
+            path.pop()
