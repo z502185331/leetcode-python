7ca1030274908facf91ec02ecf2a648e6915e63a
z502185331 <lieyongz@andrew.cmu.edu>
2016-06-21 20:04:07 -0400

    [77. Combinations][Time Limit Exceeded]committed by LeetCode Extension

--- 77-Combinations/solution.py
+++ 77-Combinations/solution.py
@@ -6,20 +6,21 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         
+        if not n or not k:
+            return []
+        
         res = []
         
         def dfs(index, path):
             if len(path) == k:
-                if path not in res:
-                    res.append(list(path))
+                res.append(list(path))
                 return
             
-            for i in xrange(index, n + 1):
+            for i in range(index, n + 1):
                 path.append(i)
                 dfs(i + 1, path)
                 path.pop()
         
         dfs(1, [])
-        
         return res
             
