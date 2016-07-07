4fd1e66f7bfc2d59460e10dc127fc8a295fba210
z502185331 <lieyongz@andrew.cmu.edu>
2016-07-07 14:31:31 -0400

    [361. Bomb Enemy][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 361-Bomb-Enemy/solution.py
@@ -0,0 +1,83 @@
+class Solution(object):
+    def maxKilledEnemies(self, grid):
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+        """
+        
+        if not grid or not grid[0]:
+            return 0
+        
+        m, n = len(grid), len(grid[0])
+        kills = [[None] * n for _ in xrange(m)]
+        
+        maxEnemy = 0
+        for i in xrange(m):
+            for j in xrange(n):
+                if grid[i][j] == 'W' or grid[i][j] == 'E':
+                    continue
+                
+                horizontalEnemy, verticalEnemy = -1, -1
+                
+                # find the enemy number in a horizontal line
+                y = j - 1
+                while y >= 0:
+                    if grid[i][y] == '0':
+                        horizontalEnemy = kills[i][y][0]
+                        break
+                    if grid[i][y] == 'W':
+                        break
+                    y -= 1
+                if horizontalEnemy == -1:
+                    horizontalEnemy = self.countHorizEnemy(grid, i, j)
+                
+                # find the enemy number in a vertical line
+                x = i - 1
+                while x >= 0:
+                    if grid[x][j] == '0':
+                        verticalEnemy = kills[x][j][1]
+                        break
+                    if grid[x][j] == 'W':
+                        break
+                    x -= 1
+                if verticalEnemy == -1:
+                    verticalEnemy = self.countVertiEnemy(grid, i, j)
+                
+                maxEnemy = max(maxEnemy, horizontalEnemy + verticalEnemy)
+                kills[i][j] = (horizontalEnemy, verticalEnemy)
+        
+        return maxEnemy
+                        
+    
+    def countVertiEnemy(self, grid, x, y):
+        count = 0
+        
+        i = x
+        while i >= 0 and grid[i][y] != 'W':
+            if grid[i][y] == 'E':
+                count += 1
+            i -= 1
+        
+        i = x
+        while i < len(grid) and grid[i][y] != 'W':
+            if grid[i][y] == 'E':
+                count += 1
+            i += 1
+        return count
+        
+        
+    def countHorizEnemy(self, grid, x, y):
+        count = 0
+        
+        i = y
+        while i >= 0 and grid[x][i] != 'W':
+            if grid[x][i] == 'E':
+                count += 1
+            i -= 1
+        
+        i = y
+        while i < len(grid[0]) and grid[x][i] != 'W':
+            if grid[x][i] == 'E':
+                count += 1
+            i += 1
+        return count
