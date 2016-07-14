003e22e58d36213c771774ad8f2d67a9c0a8d939
z502185331 <lieyongz@andrew.cmu.edu>
2016-07-13 20:32:36 -0400

    [41. First Missing Positive][Time Limit Exceeded]committed by LeetCode Extension

---
+++ 41-First-Missing-Positive/solution.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    def firstMissingPositive(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
+        if not nums:
+            return 1
+        
+        m = len(nums)
+        for i in xrange(m):
+            if i + 1 == nums[i]:
+                continue
+            
+            while i + 1 != nums[i] and nums[i] > 0 and nums[i] < m:
+                tmp = nums[i]
+                nums[i] = nums[tmp - 1]
+                nums[tmp - 1] = tmp
+            
+        for i in xrange(0, m):
+            if i + 1 != nums[i]:
+                return i + 1
+        
+        return m + 1
