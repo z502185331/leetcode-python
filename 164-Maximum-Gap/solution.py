9e0308b7936a58a23bb4ef898c34d1f645ea7ae2
z502185331 <lieyongz@andrew.cmu.edu>
2016-07-18 11:25:46 -0400

    [164. Maximum Gap][Accepted]committed by LeetCode Extension

--- 164-Maximum-Gap/solution.py
+++ 164-Maximum-Gap/solution.py
@@ -8,40 +8,35 @@ class Solution(object):
         if len(nums) < 2:
             return 0
         
+        max_num, min_num = max(nums), min(nums)
         m = len(nums)
         
-        # bucket sort
-        max_num = max(nums)
-        min_num = min(nums)
-        
-        # the number of bucket
-        # each bucket contains the local_max and local_min
         bucket_size = (max_num - min_num) / m + 1
-        bucket_len = (max_num - min_num) / bucket_size + 1
-        buckets = [None] * bucket_len
+        bucket_number = (max_num - min_num) / bucket_size + 1
+        buckets = [None] * bucket_number
         
         for num in nums:
             
-            # get the pos of num in buckets
-            i = (num - min_num) / bucket_size
-            if buckets[i] is None:  # bucket is empty
-                buckets[i] = []
-                buckets[i].append(num)
-                buckets[i].append(num)
+            # get the index of num in buckets
+            idx = (num - min_num) / bucket_size
+            
+            if buckets[idx] is None:
+                buckets[idx] = [num, num]
+            
             else:
-                if num < buckets[i][0]:
-                    buckets[i][0] = num
-                elif num > buckets[i][1]:
-                    buckets[i][1] = num
-        
-        # get the max gap
+                if buckets[idx][0] > num:
+                    buckets[idx][0] = num
+                
+                elif buckets[idx][1] < num:
+                    buckets[idx][1] = num
+            
         max_gap = 0
         prev = 0
-        for i in xrange(1, bucket_len):
+        for i in xrange(1, bucket_number):
             if buckets[i] is None:
                 continue
+            
             max_gap = max(max_gap, buckets[i][0] - buckets[prev][1])
             prev = i
-        
+            
         return max_gap
-        
